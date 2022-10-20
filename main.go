0879ae9309419f6c7d7e543f0174b45ad11363a2
griffinbird <benito.griffin@gmail.com>
2022-10-20 17:07:59 +1100

    Added returning request units and latency

--- main.go
+++ main.go
@@ -3,17 +3,28 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"time"
+	"unsafe"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-
 )
+
+type requestStatistics struct {
+	ActivityID                                  string
+	CommandName                                 string
+	EstimatedDelayFromRateLimitingInMilliseconds int
+	RequestCharge                               float64
+	RequestDurationInMilliseconds               int
+	RetriedDueToRateLimiting                    bool
+	OK                                          int
+}
 
 func main() {
 	// Set client options and connect to MongoDB
@@ -42,6 +53,7 @@ func main() {
 	database := client.Database("testDB")
 	collection := database.Collection("testCollection")
 
+	// Get Mongo verison information and print
 	buildInfoCmd := bson.D{bson.E{Key: "buildInfo", Value: 1}}
 	var buildInfoDoc bson.M
 	if err := database.RunCommand(ctx, buildInfoCmd).Decode(&buildInfoDoc); err != nil {
@@ -49,14 +61,15 @@ func main() {
 	}
 	log.Println("Database version:", buildInfoDoc["version"])
 
+	// Obtain size of the file and print
 	jsonFile, _ := os.Stat(os.Getenv("FILENAME"))
 	jsonFileSize := jsonFile.Size()
 	jsonFileSizeMB := jsonFileSize
 
 	log.Printf("The JSON file size is %d bytes\n", jsonFileSizeMB)
 
+	// Open the file and read it. 
 	fileContent, err := os.Open(os.Getenv("FILENAME"))
-
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -66,16 +79,44 @@ func main() {
 
 	defer fileContent.Close()
 
-	byteResult, _ := ioutil.ReadAll(fileContent)
+	byteResult, err := ioutil.ReadAll(fileContent)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
+	// Create a map where the key is string and the value is of type
+	// interface. interface() is an empty interface and any object can
+	// be stored in it.
 	var res map[string]interface{}
-	json.Unmarshal([]byte(byteResult), &res)
 
+	// Stores the JSON data from the file into res
+	json.Unmarshal([]byte(byteResult), &res)
+	
 	_, err = collection.InsertOne(context.TODO(), res)
 	
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("Successfully inserted into Mongo\n\n%v\n", res)	
+	//log.Printf("Successfully inserted into Mongo\n\n%v\n", res)
+	
+	statistics, err := GetLastRequestStats(client, database)
+	if err != nil {
+		log.Fatal(err)
+	}
+	requestChargeValue := fmt.Sprintf("ActivityID: %v, RequestCharge: %.2f %dms\n ", statistics.ActivityID, statistics.RequestCharge, statistics.RequestDurationInMilliseconds)
+	log.Printf("%v",requestChargeValue)
+}
+
+func GetLastRequestStats(client *mongo.Client, database *mongo.Database) (*requestStatistics, error) {
+	ctx := context.TODO()
+
+	statistics := requestStatistics{}
+	map1 := map[string]interface{}{"getLastRequestStatistics": "1"}
+	err := database.RunCommand(ctx, map1, nil).Decode(&statistics)
+	if err != nil {
+		return nil, err
+	}
+	return &statistics, nil
 }
